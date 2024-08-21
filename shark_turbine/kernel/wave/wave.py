54d5957f794df2de8898825655be1cd0889f65e5
Harsh Menon <harsh@nod-labs.com>
2024-08-21 15:59:28 -0700

    Add optimization to minimize number of global loads
    In order to minimize the number of global loads, we
    modify the expansion and allow more threads to load
    larger amounts of data. This PR adds this optimization
    pass.
    
    Also, read nodes can now have multiple write dependencies
    and the expansion pass has been modified to handle this
    case. Other utilities have also been added such as
    delinearization and removing unused nodes.
    
    Signed-off-by: Harsh Menon <harsh@nod-labs.com>

--- shark_turbine/kernel/wave/wave.py
+++ shark_turbine/kernel/wave/wave.py
@@ -18,6 +18,8 @@ from .expansion import expand_graph
 from .promotion import promote_placeholders
 from .hoisting import hoist_allocs
 from .utils import canonicalize_module
+from .minimize_global_loads import minimize_global_loads
+from .barriers import add_shared_memory_barriers
 from ..lang import Grid, IndexMapping
 from ..lang.global_symbols import *
 from ..ops import wave_ops
@@ -207,6 +209,12 @@ class LaunchableWave(Launchable):
 
         # Register analysis to determine register shapes.
         determine_register_shape(graph)
+
+        # Optimizations.
+        minimize_global_loads(graph, self.constraints)
+
+        # Add shared memory barriers.
+        add_shared_memory_barriers(graph)
 
         self.grid_type.dims = [1, 1, 1]
         for constraint in self.workgroup_constraints:
